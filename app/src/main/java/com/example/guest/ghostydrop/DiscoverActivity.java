c2fede40b46892ac809d4b827097cbb2a7be38f1
jackson Meyer <jacksonmeyer@jacksons-MacBook-Pro.local>
2016-12-21 15:58:29 -0800

    add a list view with toasts and lock in portraits

--- app/src/main/java/com/example/guest/ghostydrop/DiscoverActivity.java
+++ app/src/main/java/com/example/guest/ghostydrop/DiscoverActivity.java
@@ -1,13 +1,44 @@
 package com.example.guest.ghostydrop;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class DiscoverActivity extends AppCompatActivity {
+    @Bind(R.id.discoverListView)
+    ListView mDiscoverListView;
+
+
+    private String[] photoObjects = new String[] {"Mi Mero Mole", "Mother's Bistro",
+            "Life of Pie", "Screen Door", "Luc Lac", "Sweet Basil",
+            "Slappy Cakes", "Equinox", "Miss Delta's", "Andina",
+            "Lardo", "Portland City Grill", "Fat Head's Brewery",
+            "Chipotle", "Subway"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_discover);
+        ButterKnife.bind(this);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, photoObjects);
+        mDiscoverListView.setAdapter(adapter);
+
+        mDiscoverListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String Ghost = ((TextView)view).getText().toString();
+                Toast.makeText(DiscoverActivity.this, Ghost, Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/guest/ghostydrop/adapters/PhotoListAdapter.java
@@ -0,0 +1,8 @@
+package com.example.guest.ghostydrop.adapters;
+
+/**
+ * Created by jacksonmeyer on 12/21/16.
+ */
+
+public class PhotoListAdapter {
+}
