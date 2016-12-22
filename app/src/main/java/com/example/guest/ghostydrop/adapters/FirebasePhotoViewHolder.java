c2c7ee530d4e435dde410ee1774fecb62a51cd46
jackson Meyer <jacksonmeyer@jacksons-MacBook-Pro.local>
2016-12-21 21:06:16 -0800

    add the decode code for decoding bit map into photos along with the comments and the distance away from where that picture was taken

--- app/src/main/java/com/example/guest/ghostydrop/adapters/FirebasePhotoViewHolder.java
+++ app/src/main/java/com/example/guest/ghostydrop/adapters/FirebasePhotoViewHolder.java
@@ -1,8 +1,12 @@
 package com.example.guest.ghostydrop.adapters;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.widget.RecyclerView;
+import android.util.Base64;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.guest.ghostydrop.Constants;
@@ -13,11 +17,15 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 
 public class FirebasePhotoViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    private static final int MAX_WIDTH = 200;
+    private static final int MAX_HEIGHT = 200;
 
     View mView;
     Context mContext;
@@ -31,10 +39,31 @@ public class FirebasePhotoViewHolder extends RecyclerView.ViewHolder implements 
     public void bindPicture(Picture picture) {
         TextView PhotoComment = (TextView) mView.findViewById(R.id. photoCommentTextView);
         TextView DistanceText= (TextView) mView.findViewById(R.id.distanceTextView);
+        ImageView  Image= (ImageView) mView.findViewById(R.id. photoImageView);
+
+
+        if (!picture.getImageUrl().contains("http")) {
+            try {
+                Bitmap imageBitmap = decodeFromFirebaseBase64(picture.getImageUrl());
+                Image.setImageBitmap(imageBitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Picasso.with(mContext)
+                    .load(picture.getImageUrl())
+                    .resize(MAX_WIDTH, MAX_HEIGHT)
+                    .centerCrop()
+                    .into(Image);
+        }
 
         PhotoComment.setText(picture.getComment());
         DistanceText.setText(picture.getLatitude() + "hello" + picture.getLongitude());
+    }
 
+    public static Bitmap decodeFromFirebaseBase64(String image) throws IOException {
+        byte[] decodedByteArray = android.util.Base64.decode(image, Base64.DEFAULT);
+        return BitmapFactory.decodeByteArray(decodedByteArray, 0, decodedByteArray.length);
     }
 
     @Override
